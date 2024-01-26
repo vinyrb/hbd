f20c20ca3bedc88a013a270093171dd1c6ad6dcf
vinyrb <vinyrbe@gmail.com>
2024-01-26 11:29:44 +0700

    Happy Birthday

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Feb 9, 2024',
+  birthdate: 'Jan 26, 2024',
   name: 'Casey'
 };
 
